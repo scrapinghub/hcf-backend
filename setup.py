ca9b8bcce88930c3f96b5c5bfb853976ad89d705
Martin Olveyra <molveyra@gmail.com>
2015-03-12 16:40:29 -0200

    fix lib name

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name         = 'hcf backend',
+    name         = 'hcf_backend',
     version      = '0.1',
     packages     = find_packages(),
 )

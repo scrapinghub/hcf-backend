8dc0ff53f4b96442a50916eb9bc8d1bd3d5cc42c
Alexander Sibiryakov <sixty-one@yandex.ru>
2016-04-06 20:15:36 +0200

    states object draft using collections

--- hcf_backend/manager.py
+++ hcf_backend/manager.py
@@ -4,9 +4,57 @@ from collections import defaultdict
 
 import requests as requests_lib
 from hubstorage import HubstorageClient
+from frontera.contrib.backends.memory import MemoryStates
+from json import loads
 
 
 LOG = logging.getLogger(__name__)
+
+
+class HCFStates(MemoryStates):
+
+    def __init__(self, auth, project_id, frontier, cache_size_limit):
+        super(HCFStates, self).__init__(cache_size_limit)
+        self._hs_client = HubstorageClient(auth=auth)
+        self.projectid = project_id
+        project = self._hs_client.get_project(self.projectid)
+        self._collections = project.collections
+        self.logger = logging.getLogger("HCFStates")
+
+    def frontier_start(self):
+        self._store = self._collections.new_store("states")
+
+    def fetch(self, fingerprints):
+        to_fetch = [f for f in fingerprints if f not in self._cache]
+        self.logger.debug("cache size %s" % len(self._cache))
+        self.logger.debug("to fetch %d from %d" % (len(to_fetch), len(fingerprints)))
+
+        prepared_keys = ["key=%s" % f for f in to_fetch]
+        params = {'method':'GET',
+                  'url':'https://storage.scrapinghub.com/collections/%d/s/%s' % (self.projectid, "states"),
+                  'params':str('&').join(prepared_keys),
+                  'auth':self._hs_client.auth}
+        response = self._hs_client.session.request(**params)
+        assert response.status_code == 200
+        fprint = iter(to_fetch)
+        # TODO: fix the situations when key is absent
+        for line in response.content.split('\n'):
+            o = loads(line)
+            self._cache[fprint.next()] = o['value']
+
+    def flush(self, force_clear=False):
+        buffer = []
+        try:
+            for fprint, state_val in self._cache.iteritems():
+                buffer.append({'_key': fprint, 'value':state_val})
+                if len(buffer) > 64:
+                    self.logger.debug("Flushing %d items." % len(buffer))
+                    self._store.set(buffer)
+                    buffer = []
+        finally:
+            self._store.set(buffer)
+        self.logger.debug("State cache has been flushed.")
+        super(HCFStates, self).flush(force_clear)
 
 
 class HubstorageCrawlFrontier(object):

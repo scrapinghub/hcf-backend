7732605c765414fa3afef44c1fb4dac86d029c2d
Martin Olveyra <molveyra@gmail.com>
2016-06-17 12:39:56 -0300

    url must be passed directly in qdata, not in qdata['request']

--- hcf_backend/backend.py
+++ hcf_backend/backend.py
@@ -238,8 +238,9 @@ class HCFBackend(Backend):
         link.meta.pop('origin_is_frontier', None)
         hcf_request = {'fp': getattr(link, 'meta', {}).get('frontier_fingerprint', link.url)}
         qdata = {'request': {}}
-        for attr in ('url', 'method', 'headers', 'cookies', 'meta'):
+        for attr in ('method', 'headers', 'cookies', 'meta'):
             qdata['request'][attr] = getattr(link, attr)
+            qdata['url'] = getattr(link, 'url')
         hcf_request['qdata'] = qdata
 
         slot = self.hcf_get_producer_slot(link)

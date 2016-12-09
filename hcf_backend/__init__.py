391f1ed87e14e3b4e58b67a9c86d8947804aa357
Martin Olveyra <molveyra@gmail.com>
2016-12-09 16:05:18 -0300

    set first tagged version

--- hcf_backend/__init__.py
+++ hcf_backend/__init__.py
@@ -1,1 +1,4 @@
 from .backend import *
+
+
+__version = '0.1.0'
--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ from setuptools import setup, find_packages
 
 setup(
     name         = 'hcf_backend',
-    version      = '0.1',
+    version      = '0.1.0',
     packages     = find_packages(),
 )

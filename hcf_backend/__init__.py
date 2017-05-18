5736fbb54ba54b569b9c7590a4ec407f13e1ddb4
Martin Olveyra <molveyra@gmail.com>
2017-05-17 23:19:43 -0300

    bump version 0.1.1->0.1.2

--- hcf_backend/__init__.py
+++ hcf_backend/__init__.py
@@ -1,4 +1,4 @@
 from .backend import *
 
 
-__version = '0.1.1'
+__version = '0.1.2'
--- setup.py
+++ setup.py
@@ -4,6 +4,6 @@ from setuptools import setup, find_packages
 
 setup(
     name         = 'hcf_backend',
-    version      = '0.1.1',
+    version      = '0.1.2',
     packages     = find_packages(),
 )

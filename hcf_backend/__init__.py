f97fdf2410f86ba1cd32a4bdf62fde3cd518bc7f
Martin Olveyra <molveyra@gmail.com>
2018-08-24 21:28:47 +0000

    updated to version 0.2.2.1

--- hcf_backend/__init__.py
+++ hcf_backend/__init__.py
@@ -1,4 +1,4 @@
 from .backend import *
 
 
-__version = '0.2.2'
+__version = '0.2.2.1'
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name         = 'hcf_backend',
-    version      = '0.2.2',
+    version      = '0.2.2.1',
     packages     = find_packages(),
     install_requires=(
         'frontera==0.7.1',

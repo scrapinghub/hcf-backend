413104704a7a197650257687f1c93eb8a70e0570
Martin Olveyra <molveyra@gmail.com>
2018-08-28 15:46:26 -0300

    added license and updated setup.py for the eventuality of publishing in pypi

--- setup.py
+++ setup.py
@@ -3,15 +3,31 @@
 from setuptools import setup, find_packages
 
 setup(
-    name         = 'hcf_backend',
+    name         = 'hcf-backend',
     version      = '0.2.2.1',
+    description  = 'ScrapyCloud HubStorage frontier backend for Frontera',
+    long_description = open('README.rst').read(),
+    author_email = 'info@scrapinghub.com',
+    license      = 'BSD',
+    url          = 'https://github.com/scrapinghub/hcf-backend',
+    maintainer   ='Scrapinghub',
+    maintainer_email = 'info@scrapinghub.com',
     packages     = find_packages(),
-    install_requires=(
+    install_requires = (
         'frontera==0.7.1',
         'humanize==0.5.1',
         'requests>=2.18.4',
         'retrying>=1.3.3',
         'scrapinghub>=2.0.0',
     ),
-    scripts=["bin/hcfpal.py", "bin/hcfmanager.py"]
+    scripts = ["bin/hcfpal.py", "bin/hcfmanager.py"],
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+    ]
 )

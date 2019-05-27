1be3c6797e7de3558386f374c24e315eed2c04de
Martin Olveyra <molveyra@gmail.com>
2019-05-27 20:34:35 -0300

    update to version 0.3.4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name         = 'hcf-backend',
-    version      = '0.3.3',
+    version      = '0.3.4',
     description  = 'ScrapyCloud HubStorage frontier backend for Frontera',
     long_description = open('README.rst').read(),
     license      = 'BSD',

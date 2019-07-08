38b63c710e3fd3cf9db8dda80656f3430facd042
Martin Olveyra <molveyra@gmail.com>
2019-07-08 09:57:37 -0300

    update python version targets

--- setup.py
+++ setup.py
@@ -24,8 +24,7 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ]
 )

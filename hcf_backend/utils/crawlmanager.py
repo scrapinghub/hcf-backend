2c0d1a39926fdbef95fef846fcbe3be40843015b
Martin Olveyra <molveyra@gmail.com>
2021-06-08 16:17:56 -0300

    black formatting

--- hcf_backend/utils/crawlmanager.py
+++ hcf_backend/utils/crawlmanager.py
@@ -13,7 +13,7 @@ from shub_workflow.crawl import CrawlManager
 from hcf_backend.utils.hcfpal import HCFPal
 
 
-logging.basicConfig(format='%(message)s', level=logging.INFO)
+logging.basicConfig(format="%(message)s", level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
@@ -27,9 +27,9 @@ class HCFCrawlManager(CrawlManager):
 
     def add_argparser_options(self):
         super().add_argparser_options()
-        self.argparser.add_argument('frontier', help='Frontier name')
-        self.argparser.add_argument('prefix', help='Slot prefix')
-        self.argparser.add_argument('--frontera-settings-json')
+        self.argparser.add_argument("frontier", help="Frontier name")
+        self.argparser.add_argument("prefix", help="Slot prefix")
+        self.argparser.add_argument("--frontera-settings-json")
 
     @property
     def description(self):
@@ -38,36 +38,44 @@ class HCFCrawlManager(CrawlManager):
     def get_spider_args(self, override=None):
         if self.args.frontera_settings_json is not None:
             override = override or {}
-            override.update({
-                'frontera-settings-json': self.args.frontera_settings_json
-            })
+            override.update(
+                {"frontera-settings-json": self.args.frontera_settings_json}
+            )
         return super().get_spider_args(override)
 
     def print_frontier_status(self):
         result = self.hcfpal.get_slots_count(self.args.frontier, self.args.prefix)
         logger.info(f"Frontier '{self.args.frontier}' status:")
-        for slot in sorted(result['slots'].keys()):
-            cnt_text = '\t{}: {}'.format(slot, result['slots'][slot])
+        for slot in sorted(result["slots"].keys()):
+            cnt_text = "\t{}: {}".format(slot, result["slots"][slot])
             logger.info(cnt_text)
-        logger.info('\tTotal count: {}'.format(humanize.intcomma(result['total'])))
+        logger.info("\tTotal count: {}".format(humanize.intcomma(result["total"])))
 
-        return set(result['slots'].keys())
+        return set(result["slots"].keys())
 
     def workflow_loop(self):
         available_slots = self.print_frontier_status()
 
         running_jobs = 0
-        states = 'running', 'pending'
+        states = "running", "pending"
         for state in states:
-            for job in self.get_project().jobs.list(spider=self.args.spider, state=state, meta='spider_args'):
-                frontera_settings_json = json.loads(job['spider_args'].get('frontera_settings_json', '{}'))
-                if 'HCF_CONSUMER_SLOT' in frontera_settings_json:
-                    slot = frontera_settings_json['HCF_CONSUMER_SLOT']
+            for job in self.get_project().jobs.list(
+                spider=self.args.spider, state=state, meta="spider_args"
+            ):
+                frontera_settings_json = json.loads(
+                    job["spider_args"].get("frontera_settings_json", "{}")
+                )
+                if "HCF_CONSUMER_SLOT" in frontera_settings_json:
+                    slot = frontera_settings_json["HCF_CONSUMER_SLOT"]
                     if slot in available_slots:
                         available_slots.discard(slot)
                         running_jobs += 1
 
-        available_slots = [slot for slot in available_slots if self.hcfpal.get_slot_count(self.args.frontier, slot) > 0]
+        available_slots = [
+            slot
+            for slot in available_slots
+            if self.hcfpal.get_slot_count(self.args.frontier, slot) > 0
+        ]
         logger.info(f"Available slots: {available_slots!r}")
         if available_slots:
             random.shuffle(available_slots)
@@ -78,17 +86,27 @@ class HCFCrawlManager(CrawlManager):
                     logger.info("Already running max number of jobs.")
 
             for slot in available_slots:
-                frontera_settings_json = json.dumps({
-                    'HCF_CONSUMER_SLOT': slot,
-                    'HCF_CONSUMER_FRONTIER': self.args.frontier,
-                })
-                logger.info(f"Will schedule spider job with frontera settings {frontera_settings_json}")
-                jobkey = self.schedule_spider(spider_args_override={'frontera_settings_json': frontera_settings_json})
-                logger.info(f"Scheduled job {jobkey} with frontera settings {frontera_settings_json}")
+                frontera_settings_json = json.dumps(
+                    {
+                        "HCF_CONSUMER_SLOT": slot,
+                        "HCF_CONSUMER_FRONTIER": self.args.frontier,
+                    }
+                )
+                logger.info(
+                    f"Will schedule spider job with frontera settings {frontera_settings_json}"
+                )
+                jobkey = self.schedule_spider(
+                    spider_args_override={
+                        "frontera_settings_json": frontera_settings_json
+                    }
+                )
+                logger.info(
+                    f"Scheduled job {jobkey} with frontera settings {frontera_settings_json}"
+                )
             return True
         return bool(running_jobs)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     manager = HCFCrawlManager()
     manager.run()
